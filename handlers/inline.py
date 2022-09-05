2e391beacc8d8094f61e41740e14dff81331df4d
sanbekov <ajteginasanbekov6@gmail.com>
2022-09-05 19:00:26 +0300

    added wikipedia

--- handlers/inline.py
+++ handlers/inline.py
@@ -25,5 +25,23 @@ async def inline_youtube_handler(query: types.InlineQuery):
     await query.answer(articles, cache_time=60, is_personal=True)
 
 
+async def inline_wikipedia_handler(query: types.InlineQuery):
+    text = query.query or "echo"
+    link = f"https://www.google.com/search?q={text}"
+    result_id: str = hashlib.md5(text.encode()).hexdigest()
+    articles = [
+        types.InlineQueryResultArticle(
+            id=result_id,
+            title= "wikipedia:",
+            url=link,
+            thumb_url=f"{link['thumbnails'][0]}",
+            input_message_content=types.InputMessageContent(
+                message_text=link
+            )
+        )
+    ]
+    await query.answer(articles, cache_time=60, is_personal=True)
+
 def register_handler_inline(dp: Dispatcher):
-    dp.register_inline_handler(inline_youtube_handler)
+    #dp.register_inline_handler(inline_youtube_handler)
+    dp.register_inline_handler(inline_wikipedia_handler)
